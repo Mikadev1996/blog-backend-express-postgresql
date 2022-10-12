02ef05b05a4c1d502392c74fab4926ef0857308d
Mika.dev <charmikadev@gmail.com>
2022-10-13 00:05:16 +0100

    updated db to Azure SQL server

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'blog',
-    host: 'database',
-    database: 'blog',
-    password: 'blog1996',
+    user: 'blog@blog-express-database',
+    host: 'blog-express-database.postgres.database.azure.com',
+    database: 'postgres',
+    password: process.env.SQL_PASS,
+    ssl: true
 })
 
 module.exports = pool;
